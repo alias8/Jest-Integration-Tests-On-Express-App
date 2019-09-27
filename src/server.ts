e79bfafdc09e441d1cb0887397fcba57b7f56645
James Kirk <jkirk@switch.tv>
2019-09-27 16:42:10 +1000

    wip

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,13 @@ import yargs from "yargs";
 import App from "./app";
 import { UserController } from "./controllers/userController";
 
-console.log(`args in server.ts:`);
-console.log(yargs.argv);
 dotenv.config({
-    path: path.resolve(__dirname, "config", yargs.argv.env as string, ".env")
+    path: path.resolve(
+        __dirname,
+        "config",
+        yargs.argv.configEnvironment as string,
+        ".env"
+    )
 });
 
 export const app = new App([new UserController()]);
--- src/test/math.test.ts
+++ src/test/math.test.ts
@@ -1,89 +1,24 @@
-import dotenv from "dotenv";
-import path from "path";
+import express from "express";
 import request from "supertest";
-import App from "../app";
+import { app } from "../server";
 
-// test("example", () => {
-//     const appExample = express();
-//
-//     appExample.get("/user", (req, res) => {
-//         res.status(200).json({ name: "john" });
-//     });
-//     request(appExample)
-//         .get("/user")
-//         .expect("Content-Type", /json/)
-//         .expect("Content-Length", "15")
-//         .expect(200)
-//         .end((err, res) => {
-//             if (err) {
-//                 throw err;
-//             }
-//         });
-// });
-//
-// test("example2", () => {
-//     class App {
-//         public app: express.Application;
-//
-//         constructor() {
-//             this.app = express();
-//
-//             this.app.get("/user", (req, res) => {
-//                 res.status(200).json({ name: "john" });
-//             });
-//         }
-//     }
-//
-//     const appNotImported = new App();
-//     request(appNotImported.app)
-//         .get("/user")
-//         .expect("Content-Type", /json/)
-//         .expect("Content-Length", "15")
-//         .expect(200)
-//         .end((err, res) => {
-//             if (err) {
-//                 throw err;
-//             }
-//         });
-// });
-//
-// test.skip("example3", () => {
-//     request(app.app)
-//         .get("/user")
-//         .expect("Content-Type", /json/)
-//         .expect("Content-Length", "15")
-//         .expect(200)
-//         .end((err, res) => {
-//             if (err) {
-//                 throw err;
-//             }
-//         });
-// });
-//
-// test("example4", () => {
-//     request(app.app)
-//         .get("/user")
-//         .expect("Content-Type", /json/)
-//         .expect("Content-Length", "15")
-//         .expect(200)
-//         .end((err, res) => {
-//             if (err) {
-//                 throw err;
-//             }
-//         });
-// });
+test("example", () => {
+    const appExample = express();
 
-let app: App;
-
-beforeEach(() => {
-    // will this work?
-    jest.resetModules();
-    dotenv.config({ path: path.resolve(__dirname, "config", "test", ".env") });
-    app = require("../server");
-    console.log(`environment in beforeEach: ${process.env.NODE_ENV}`);
+    appExample.get("/user", (req, res) => {
+        res.status(200).json({ name: "john" });
+    });
+    request(appExample)
+        .get("/user")
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                throw err;
+            }
+        });
 });
 
-test("example5", () => {
+test("example3", () => {
     request(app.app)
         .get("/user")
         .expect("Content-Type", /json/)
